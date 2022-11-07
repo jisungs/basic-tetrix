ff3fff5c6e809c6369ee1a98639d3a0b3f8d300b
Jisung Smart <jisung.smart@gmail.com>
2022-11-07 21:11:22 +0900

    add movedown and freeze function to app.js

--- app.js
+++ app.js
@@ -58,10 +58,39 @@ document.addEventListener("DOMContentLoaded", ()=>{
     })
   }
 
+draw()
 function undraw() {
     current.forEach(index => {
         squares[currentPosition + index].classList.remove('tetromino')
     })
 }
+
+//make the tetromino move down every second
+timerId = setInterval(moveDown, 500)
+
+//move down function
+function moveDown(){
+  undraw()
+  currentPosition += width
+  draw()
+  freeze()
+}
+
+function freeze(){
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      //start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+    }
+  }
+
+
+
+
+
+
 
 })// end of addEventListner

adf57a0c27fdb214b981b2f9e1aed6e3bbc2a441
Jisung Smart <jisung.smart@gmail.com>
2022-11-07 12:55:26 +0900

    Add variable to js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelector('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+    
+    console.log(squares)
 
 })

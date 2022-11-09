340d6ee11c3579cbc978015c84c8f2452b933a14
Jisung Smart <jisung.smart@gmail.com>
2022-11-09 15:36:01 +0900

    Add rotate function

--- app.js
+++ app.js
@@ -127,6 +127,22 @@ function moveRight() {
   draw()
 }
 
+//rotate the tetromino
+function rotate() {
+  undraw()
+  currentRotation ++
+  if(currentRotation === current.length) { //if the current rotation gets to 4, make it go back to 0
+    currentRotation = 0
+  }
+  current = theTetrominoes[random][currentRotation]
+  checkRotatedPosition()
+  draw()
+}
+
+  //show up-next tetromino in mini-grid display
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  const displayIndex = 0
 
 
 
